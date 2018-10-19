d92b4cd9c10928daf092d68ffb3d4c2b91783088
A. Wolkers <awolkers@mirabeau.nl>
2018-10-19 10:33:22 +0200

    Compass: added variable wind direction

--- src/components/compass/Compass.js
+++ src/components/compass/Compass.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { windDegreesToCompass } from '../../utils/weatherHelpers'
 
 const Compass = ({ degrees }) => {
-    const direction = windDegreesToCompass(degrees)
-    const style = {
-        transform: `rotate(${degrees - 90}deg)`
+    if (degrees) {
+        const direction = windDegreesToCompass(degrees)
+        const style = {
+            transform: `rotate(${degrees - 90}deg)`
+        }
+
+        return (
+            <div className="compass" style={style} aria-label={direction}>
+                {direction} {degrees}
+            </div>
+        )
     }
 
-    return (
-        <div className="compass" style={style} aria-label={direction}>
-            {direction} {degrees}
-        </div>
-    )
+    return <div className="compass compass--rotating" />
 }
 
 export default Compass
