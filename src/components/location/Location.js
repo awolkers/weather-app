559489266d17c5b25f1675229a6c0aa290240ddc
A. Wolkers <awolkers@mirabeau.nl>
2018-10-19 09:20:28 +0200

    location detail page tweaks

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,13 +1,50 @@
 import React, { Fragment } from 'react'
+import { formatTemp, formatWindSpeed } from '../../utils/weatherHelpers'
+import Compass from '../compass/Compass'
 
 class Location extends React.Component {
     render() {
-        return (
-            <Fragment>
-                <h2>Location Dashboard</h2>
-                <p>{this.props.locationId}</p>
-            </Fragment>
-        )
+        // console.log('render')
+        const { location, settings, getLocationForecast } = this.props
+        console.log(location)
+
+        if (location) {
+            return (
+                <Fragment>
+                    <h2>
+                        <a
+                            href={`https://www.google.com/maps/@${
+                                location.coord.lat
+                            },${location.coord.lon},13z`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {location.name}, {location.sys.country}
+                        </a>
+                    </h2>
+
+                    <img
+                        src={`http://openweathermap.org/img/w/${
+                            location.weather[0].icon
+                        }.png`}
+                        className="locationlist__link-icon"
+                        alt={`icon ${location.weather[0].description}`}
+                        width="50"
+                        height="50"
+                    />
+                    {location.weather[0].description}
+                    <ul>
+                        <li>{formatTemp(location.main.temp, settings)}</li>
+                        <li>
+                            {formatWindSpeed(location.wind.speed, settings)}{' '}
+                            <Compass degrees={location.wind.deg} />
+                        </li>
+                    </ul>
+                </Fragment>
+            )
+        } else {
+            return <Fragment />
+        }
     }
 }
 
